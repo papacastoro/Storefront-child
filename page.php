3f97a4c04a66751aa015a9bca92a8cd963ef8cb8
papacastoro <rmarella88@gmail.com>
2015-06-18 22:05:56 +0200

    file base

---
+++ page.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package storefront
+ */
+
+get_header();
+?>
+
+	<div id="primary" <ul class="products">
+	<?php
+		$args = array(
+			'post_type' => 'product',
+			'per_page' => 3,
+					);
+		
+		/*$args = array(
+				'columns' => '2',
+				'orderby' => 'title',
+				'order' => 'asc'
+		);*/
+		
+		$loop = new WP_Query( $args );
+		if ( $loop->have_posts() ) {
+			while ( $loop->have_posts() ) : $loop->the_post();
+				woocommerce_get_template_part( 'content', 'product' );
+			endwhile;
+		} else {
+			echo __( 'Nessun prodotto' );
+		}
+		wp_reset_postdata();
+	?>
+</ul>
+</div>
+
---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<?php
+$posts = get_posts('numberposts=1&order=ASC&orderby=post_title');
+foreach ($posts as $post) : setup_postdata( $post ); ?>
+<div style="font-weight:900"><?php the_title(); ?></div>  
+<?php the_content(); ?>
+<?php
+endforeach;
+?>
+
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @package storefront
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<div id="secondary" class="widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+<?php 
+
+$args = array('per_page' => 3);
+do_action(''); 
+?>
+<!-- #secondary -->
+</div>
