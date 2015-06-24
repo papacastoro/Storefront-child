cb066ee58b9f932913d1744acca5073e504d8b93
papacastoro <rmarella88@gmail.com>
2015-06-24 10:53:30 +0200

    1

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php if (function_exists('register_sidebar')){
 		register_sidebar( array(
-		'name'          => ('Sidebar2'),
+		'name'          => __( 'Sidebar2', 'storefront' ),
 		'id'            => 'sidebar2',
 		'description'   => '',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
--- sidebar2.php
+++ sidebar2.php
@@ -11,7 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 ?>
 
 <div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php dynamic_sidebar( 'sidebar2' ); ?>
 </div><!-- #secondary -->
 
 
